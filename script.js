3f4f12048fb4106d5586cdba9298c5f1209b4217
Albertoss10 <alberto.saiz@hotmail.es>
2023-11-21 14:00:33 +0100

    update

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const menu = document.querySelector(".menu");
+const toggle = document.querySelector(".toggle");
+toggle.addEventListener("click",()=>{
+  menu.classList.toggle("active");
+})
+
 $('.portfolio-button').mousedown(function(){
     timeout = setInterval(function(){
         window.scrollBy(0,-1); // May need to be -1 to go down
